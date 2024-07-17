122fb26dd290a60e8fbb7671ca27d8b8c9ef2bbd
your-cheonsa <ysaluse@gmail.com>
2024-07-17 19:16:08 +0800

    Delete jewelryfinal/addtocart directory

--- jewelryfinal/addtocart/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost"; // Change to your database server name
-$username = "root"; // Change to your database username
-$password = ""; // Change to your database password
-$dbname = "jewelry"; // Change to your database name
-
-$conn = new mysqli("$servername", $username, $password, $dbname);
-?>
